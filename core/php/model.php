0e619a400567f8c9dca7823a686b2bd5640d26ed
kowaluj <kowaluki1@gmail.com>
2022-08-26 02:05:16 +0200

    Create menu module

--- core/php/model.php
+++ core/php/model.php
@@ -5,6 +5,85 @@
 
     }
 
+    class myMenu {
+        private string $firstMarkup = "ul";
+        private string $secondMarkup = "li";
+        private string $thirdMarkup = "a";
+
+        //Example menu
+        private array $menu = array(
+            ["Home page","noMore","http://127.0.0.1/strony/praca/"],
+            ["About", "more",
+                [
+                    ["About Us","noMore","http://127.0.0.1/strony/praca/AboutUs"],
+                    ["About App","noMore","http://127.0.0.1/strony/praca/AboutApp"],
+                    ["Contact","more",
+                        [
+                            ["Via e-mail","noMore","mailto:kowaluki1@gmail.com"],
+                            ["Via phone","noMore","tel:+48795397851"]
+                        ]
+                    ]
+                ]
+            ],
+            ["Portfolio","noMore","http://127.0.0.1/strony/praca/portfolio"],
+        );
+
+        //Add menu or update
+        public function addMenu(array $menu, bool $add = false) :void {
+            $add === true ? array_push($this->menu, $menu): $this->menu = $menu;
+        }
+
+        //Download menu from other source: GET or POST
+        public function downloadMenu(string $url = "/downloadData", bool $add = false, array $data = []) :void {
+            count($data)==0 ? $response = http_get($url): $response = httpPost($url, $data);
+            $this->addMenu($response,$add);
+        }
+
+        //Changing default markups for presenting data in other ways (default: ul, li), examples in index.php
+        public function changeMarkups(string $check, string $newValue = "") {
+            switch($check) {
+                case "first":
+                    $this->firstMarkup = $newValue;
+                break;
+                case "second":
+                    $this->secondMarkup = $newValue;
+                break;
+                case "third":
+                    $this->thirdMarkup = $newValue;
+                break;
+                case "default":
+                    $this->firstMarkup = "ul";
+                    $this->secondMarkup = "li";
+                    $this->thirdMarkup = "a";
+            }
+        }
+
+        //Creating menu from Array to String
+        public function createMenu(string $type = "numeric") { //CSS
+            $first = "<$this->firstMarkup style='list-style-type:$type;'>";
+            $menu = $first;
+            $menu .= $this->subset($this->menu);
+            $menu .= "</$this->firstMarkup>";
+            return $menu;
+        }
+
+        //List nesting
+        private function subset($menu) { //
+            $return = "";
+            foreach($menu as $subset) {
+                if($subset[1]==="more") {
+                    $return .= "<$this->secondMarkup>".$subset[0]."<$this->firstMarkup>";
+                    $return .= $this->subset($subset[2]);
+                    $return .= "</$this->firstMarkup></$this->secondMarkup>";
+                }
+                elseif($subset[1]==="noMore") {
+                    $return .= "<$this->thirdMarkup href='".$subset[2]."'><$this->secondMarkup>".$subset[0]."</$this->secondMarkup></$this->thirdMarkup>";
+                }
+            }
+            return $return;
+        }
+    }
+
     //Special calculator to calculate the valuation for the cleaned area
 
     class calcClient {
@@ -54,6 +133,18 @@
         // public function  oblicz() {
         //     calc();
         // }
+    }
+    
+    function httpPost($url, $data)
+    {
+        //$data = array('key1' => 'value1', 'key2' => 'value2');
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($curl);
+        curl_close($curl);
+        return $response;
     }
 
 
--- index.php
+++ index.php
@@ -6,10 +6,49 @@
 
 
     use controller\routing;
-    $routing = new routing();
 
-    // $routing->changeUrl("http://127.0.0.1/strony/praca/12345");
-    $routing->launch();
+    use model\myMenu;
+
+    // $menu = new myMenu();
+    // $newMenuAdd =["RODO","noMore","RODO"];
+    // $newMenuAddSecond = ["Whatever with other:", "more",
+    //         [
+    //             ["Other 1", "noMore","other1"],
+    //             ["Other 2", "noMore","other2"],
+    //             ["Other 3", "noMore","other3"]
+    //         ]
+    // ];
+    // $newMenuInOne = [
+    //     ["RODO","noMore","RODO"],
+    //     ["Whatever with other:", "more",
+    //         [
+    //             ["Other 1", "noMore","other1"],
+    //             ["Other 2", "noMore","other2"],
+    //             ["Other 3", "noMore","other3"]
+    //         ]
+    //     ]
+    // ];
+    // $menu->addMenu($newMenuAdd,true);
+    // $menu->addMenu($newMenuAddSecond,true);
+    // $end = $menu->createMenu();
+    // echo $end;
+    // $end = $menu->addMenu($newMenuInOne);
+    // echo $end;
+    // foreach($newMenuInOne as $neew) {
+    //     $menu->addMenu($neew,true);
+    // }
+    // $menu->changeMarkups("first","hr");
+    // $menu->changeMarkups("second","p");
+    // $end = $menu->createMenu();
+    // echo $end;
+    // $menu->changeMarkups("default");
+    // $menu->changeMarkups("third","address");
+    // $end = $menu->addMenu($newMenuInOne);
+    // $end = $menu->createMenu();
+    // echo $end;
+    // $routing = new routing();
+    // // $routing->changeUrl("http://127.0.0.1/strony/praca/12345");
+    // $routing->launch();
     // $area = ["flooring","brick"];
     // $calc = new calcClient($area,1111);
     // $calc->echoP(); 
